4744e50490f7952dcb8ecdfe091b70d02d28dc1b
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-21 11:38:16 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+var kata = "Semangat..."
+
+console.log(kata.repeat(5))
+console.log()
+
+var bintang = "*"
+console.log(bintang.repeat(5))
+console.log(bintang.repeat(15))
+console.log(bintang.repeat(25))
+console.log(bintang.repeat(35))
+console.log(bintang.repeat(55))
+console.log(bintang.repeat(35))
+console.log(bintang.repeat(25))
+console.log(bintang.repeat(15))
+console.log(bintang.repeat(5))
+
